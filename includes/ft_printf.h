3cbeedd0483b113136f263f583015cfd60ff1189
Vallium <aalliot@student.42.fr>
2015-02-17 14:58:54 +0100

    printf on adore

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -10,4 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdarg.h>
+# include <unistd.h>
+
+# endif
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -10,4 +10,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+void		ft_putchar(char c)
+{
+	if (c)
+		write(1, &c, 1);
+}
+
+void		ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int			print_char(va_list flags)
+{
+	char	c;
+
+	c = va_arg(flags, char);
+	ft_putchar(c);
+	return (1);
+}
+
+int			print_str(va_list flags)
+{
+	char	*str;
+
+	str = va_arg(flags, char *);
+	ft_putstr(str);
+	return (1);
+}
+
+int			choose_flag(va_list flags, char c)
+{
+	if (c == 'c')
+		ft_printchar(flags);
+	else if (c == '%')
+		ft_putchar(c);
+	return (0);
+}
+
+int			ft_printf(const char *str, ...)
+{
+	va_list		flags;
+
+	va_start(flags, str);
+	while(*str)
+	{
+		if(*str == '%')
+		{
+			str++;
+			choose_flag(flags, *str);
+		}
+		else if (*str != '%')
+			ft_putchar(*str);
+		str++;
+	}
+	va_end(flags);
+	return (0);
+}
